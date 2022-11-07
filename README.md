f21b6ab28b34acc990db45d77d3b683d22cdfdf3
Truc Nguyen <truc.nguyentrung@hotmail.com>
2022-11-07 23:10:02 +0000

    init

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# identityserver-angular-sample
+## OIDC ASPNET Core + Angular
+
+### Terminology
+#### Users
+- Human
+#### Clients
+- Angular/React app, mobile app, desktop app, background services
+  - M2M (machine to machine): background services, daemon server with no user wants to call API (Client Credentials Flow)
+  - Interactive app: SPAs, native/mobile apps with user (Authorization Code Flow)
+    - id_token: details about authentication user
+    - access_token: authorization header to call API
+    - refresh_token: lifetime management for access_token
+#### Resource
+- Something to procted with Identity Server (APIs)
+- Unique name and clients use this name to specify which data they want to access
+#### Identity Token
+- Claims returned after authentication success
+#### Access Token
+- Access API by forwarding this to API
+### References
+- https://docs.duendesoftware.com/identityserver/v6/overview/terminology/
---
+++ identityserver/Config.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using Duende.IdentityServer.Models;
+
+namespace IdentityServer;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[]
+        {
+            new ApiScope("scope1"),
+            new ApiScope("scope2"),
+            new ApiScope("resource_api"),
+        };
+
+    public static IEnumerable<Client> Clients =>
+        new Client[]
+        {
+            // m2m client credentials flow client
+            new Client
+            {
+                ClientId = "m2m.client",
+                ClientName = "Client Credentials Client",
+
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
+
+                AllowedScopes = { "scope1" }
+            },
+
+            // interactive client using code flow + pkce
+            new Client
+            {
+                ClientId = "interactive",
+                ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+
+                AllowedGrantTypes = GrantTypes.Code,
+
+                RedirectUris = { "https://localhost:44300/signin-oidc" },
+                FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
+                PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
+
+                AllowOfflineAccess = true,
+                AllowedScopes = { "openid", "profile", "scope2" }
+            },
+
+            new Client
+            {
+                ClientId = "spa",
+                ClientSecrets = { new Secret("secret".Sha256()) },
+                AllowedGrantTypes = GrantTypes.Code,
+                AllowAccessTokensViaBrowser = true,
+                AllowedCorsOrigins = new List<string> { "http://localhost:4200" },
+                RequireClientSecret = false,
+                RedirectUris = { "http://localhost:4200" },
+                FrontChannelLogoutUri = "http://localhost:4200",
+                PostLogoutRedirectUris = { "http://localhost:4200" },
+                AllowOfflineAccess = true,
+                AllowedScopes = { "openid", "profile", "resource_api" }
+            },
+        };
+}
